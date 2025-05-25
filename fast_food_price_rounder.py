df1179d74f12008f10bfbd0354a19ff24534eb93
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-25 07:30:03 +0500

    bajarildi

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -1,1 +1,10 @@
-
+from num2words import num2words
+a=float(input("birinchi mahsulot narxini kiriting:($)"))
+b=float(input("ikkinchi mahsulot narxini kiriting:($)"))
+c=float(input("uchunchi mahsulot narxini kiriting:($)"))
+jami=a+b+c
+a=f"{jami:.2f}"
+print("umumiy summa:($)",jami,",",end="")
+print(num2words(jami,lang="en"),",",(num2words(jami,lang="ru")))
+print("yaxlitlangan narx:($)",f"{jami:.2f}",",",end="")
+print(num2words(a,lang="en"),",",(num2words(a,lang="ru")))

44da6bab2556e70c969eb1f32812d4eb818775d0
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-25 09:07:07 +0500

    bajarildi

--- average_spending_tracker.py
+++ average_spending_tracker.py
@@ -1,1 +1,12 @@
-
+from num2words import num2words
+a_1=float(input("birinchi kun harajatni kiriting: $)"))
+a_2=float(input("ikkinchi kun harajatini kiriting: $"))
+a_3=float(input("uchunchi kun harajatini kiriting: $"))
+a_4=float(input("to'rtinchi kun harajatini kiriting: $"))
+a_5=float(input("beshinchi kun harajatini kiriting: $"))
+a_6=float(input("oltinchi kun harajatini kiriting: $"))
+a_7=float(input("yettinchi kun harajatini kiriting: $"))
+ortachasi=(a_1+a_2+a_3+a_4+a_5+a_6+a_7)/7
+a=f"{ortachasi:.2f}"
+print("o'rtacha harajat:($)",a,",",end="")
+print(num2words(a,lang="en"),",",num2words(a,lang="ru"))

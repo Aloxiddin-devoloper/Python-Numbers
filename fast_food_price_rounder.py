d2452f085c0436917340d5b90714cc4d750ef2a4
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-25 10:55:38 +0500

    qayta topshirildi

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -4,7 +4,15 @@ b=float(input("ikkinchi mahsulot narxini kiriting:($)"))
 c=float(input("uchunchi mahsulot narxini kiriting:($)"))
 jami=a+b+c
 a=f"{jami:.2f}"
+
 print("umumiy summa:($)",jami,",",end="")
-print(num2words(jami,lang="en"),",",(num2words(jami,lang="ru")))
-print("yaxlitlangan narx:($)",f"{jami:.2f}",",",end="")
-print(num2words(a,lang="en"),",",(num2words(a,lang="ru")))
+print(num2words(jami,lang="en",to="currency"),",",end="  ")
+print((num2words(jami,lang="ru",to="currency")))
+
+
+print("yaxlitlangan narx:($)",a,",",end="")
+print(num2words(a,lang="en",to="currency"),",",end=" ")
+print(num2words(a,lang="ru",to="currency"))
+
+
+

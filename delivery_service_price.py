4d2dff9ad3da3b1cbc1a390b73ebce2f9201d5b9
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-25 10:04:47 +0500

    bajarildi

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,1 +1,9 @@
-
+from num2words import num2words
+boshlangich_tolov=5.00
+har_km_uchun=0.80
+masofa=float(input("masofani kiriting:(km)"))
+jami=boshlangich_tolov+(masofa*har_km_uchun)
+a=f"{jami:.2f}"
+print("yetkazib berish narxi:($)",a,",",end="")
+print(num2words(a,lang="en",to="currency"),"  ",end="")
+print(num2words(a,lang="ru",to="currency"))

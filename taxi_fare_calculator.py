7996684dd555ca75e0f4ec68fe00584bc5be6ac8
Aloxiddin-devoloper <aloxiddinyogmirov1@gmail.com>
2025-05-25 10:00:07 +0500

    bajarildi

--- taxi_fare_calculator.py
+++ taxi_fare_calculator.py
@@ -1,1 +1,9 @@
-
+from num2words import num2words
+boshlangich_tolov=3.00
+har_km_uchun=1.20
+masofa=float(input("masofani kiriting:(km)"))
+jami=boshlangich_tolov+(masofa*har_km_uchun)
+a=f"{jami:.2f}"
+print("taxi narxi:($)",a,",",end="")
+print(num2words(a,lang="en",to="currency"),"  ",end="")
+print(num2words(a,lang="ru",to="currency"))
